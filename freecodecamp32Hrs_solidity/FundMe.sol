d7537c20d6a5ca5a878d8b314c71da9a1280de66
Remix Workspace <>
2022-10-09 06:23:57 -0700

    Sending ethers and modifiers

--- freecodecamp32Hrs_solidity/FundMe.sol
+++ freecodecamp32Hrs_solidity/FundMe.sol
@@ -4,11 +4,15 @@
 
 // SPDX-License-Identifier: MIT
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "./PriceConverter.sol"
+import "./PriceConverter.sol";
 pragma solidity  ^0.8.8;
 
 contract FundMe{
-
+    address public  owner;
+    constructor(){
+        // gets called when deployed
+        owner = msg.sender;
+    }
     using PriceConverter for uint256;
     address[] public funders;
     mapping(address=>uint256) public addressToAmountFunded;
@@ -22,7 +26,29 @@ contract FundMe{
     }
 
     
-    // function withdraw(){
+    function withdraw() public onlyOwner{
+            for(uint256 funderIndex=0;funderIndex<funders.length; funderIndex=funderIndex+1){
+                address funder=funders[funderIndex];
+                addressToAmountFunded[funder]=0;
+                funders=new address[](0);
 
-    // }
+                // Transfering Eth
+                //1. transfer
+                payable(msg.sender).transfer(address(this).balance);
+                //2. send
+                bool sendSuccess =  payable(msg.sender).send(address(this).balance);
+                require(sendSuccess,"Send Failed"); //revert
+
+                //3. call
+                //lower level command
+                (bool callSuccess , bytes memory dataReturned)=payable(msg.sender).call{value:address(this).balance}("");
+                require(callSuccess,"Call Failed"); 
+             
+            }
+    }
+
+    modifier onlyOwner {
+        require (msg.sender==owner , "Sender is not an owner");
+        _;
+    }
 }
--- freecodecamp32Hrs_solidity/PriceConverter.sol
+++ freecodecamp32Hrs_solidity/PriceConverter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity  ^0.8.8;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-contract PriceConverter{
+library PriceConverter{
     function getPrice() internal view returns(uint256){
         // ABI
         // Address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
