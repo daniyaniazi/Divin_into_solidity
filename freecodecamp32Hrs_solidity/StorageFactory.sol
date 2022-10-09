867dab8ac289b1d9d824f9d50b9efe9f6b3ab193
Remix Workspace <>
2022-10-09 00:53:59 -0700

    Interacting with other contracts

--- freecodecamp32Hrs_solidity/StorageFactory.sol
+++ freecodecamp32Hrs_solidity/StorageFactory.sol
@@ -4,10 +4,27 @@ pragma solidity ^0.8.0;
 
 import "./basics.sol";
 contract StorageFatory{
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public simpleStorageArray;
     function createSimpleStorageContract () public {
         // how its know what simple storage contract look alike?
-        simpleStorage = new SimpleStorage();
-
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
     }
+
+    // function to access store in simple storage contract
+    function sfStore(uint256 _simpleStorageIndex , uint256 _simpleStorageNumber) public{
+        // To interact with contarct you need : 
+        // 1. Address
+        // 2. ABI - appkication Binary Interface
+
+        SimpleStorage simpleStorage= SimpleStorage(simpleStorageArray[_simpleStorageIndex]);
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    // function to read
+     function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+         SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+         return simpleStorage.retrieve();
+     }
+
 }
